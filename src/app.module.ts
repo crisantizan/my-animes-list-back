2237f99a204352d1f546af4b8b4c588a5d95d027
Cristian Santiz <crisantizan@gmail.com>
2020-11-22 21:57:51 -0500

    refactor: mongo container

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://mongo/my-animes-list-back'),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
